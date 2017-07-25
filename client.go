27e38a1a71eb6e4e11b7dfc11b3556676260fc96
Bram Vandenbogaerde <bram.vandenbogaerde@gmail.com>
2017-07-25 19:59:19 +0200

    Allow the user to provide a way to opt-out of buffering

--- client.go
+++ client.go
@@ -1,10 +1,12 @@
 package scp
 
 import (
+	"bytes"
 	"fmt"
 	"golang.org/x/crypto/ssh"
 	"io"
 	"io/ioutil"
+	"os"
 	"path"
 )
 
@@ -28,18 +30,31 @@ func (a *Client) Connect() error {
 	return nil
 }
 
-// Copies the contents of an io.Reader to a remote location
+//Copies the contents of an os.File to a remote location, it will get the length of the file by looking it up from the filesystem
+func (a *Client) CopyFromFile(file os.File, remotePath string, permissions string) {
+	stat, _ := file.Stat()
+	a.Copy(&file, remotePath, permissions, stat.Size())
+}
+
+// Copies the contents of an io.Reader to a remote location, the length is determined by reading the io.Reader until EOF
+// if the file length in know in advance please use "Copy" instead
 func (a *Client) CopyFile(fileReader io.Reader, remotePath string, permissions string) {
 	contents_bytes, _ := ioutil.ReadAll(fileReader)
-	contents := string(contents_bytes)
+	bytes_reader := bytes.NewReader(contents_bytes)
+
+	a.Copy(bytes_reader, remotePath, permissions, int64(len(contents_bytes)))
+}
+
+// Copies the contents of an io.Reader to a remote location
+func (a *Client) Copy(r io.Reader, remotePath string, permissions string, size int64) {
 	filename := path.Base(remotePath)
 	directory := path.Dir(remotePath)
 
 	go func() {
 		w, _ := a.Session.StdinPipe()
 		defer w.Close()
-		fmt.Fprintln(w, "C"+permissions, len(contents), filename)
-		fmt.Fprintln(w, contents)
+		fmt.Fprintln(w, "C"+permissions, size, filename)
+		io.Copy(w, r)
 		fmt.Fprintln(w, "\x00")
 	}()
 
