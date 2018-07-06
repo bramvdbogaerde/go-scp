77103c3a230ee33164639918a13c06fcc2b632f7
lcd1232 <8745863+lcd1232@users.noreply.github.com>
2018-07-06 17:11:56 +0300

    fix error with scp, add timeout

--- scp.go
+++ scp.go
@@ -3,6 +3,7 @@ package scp
 
 import (
 	"golang.org/x/crypto/ssh"
+	"time"
 )
 
 // Returns a new scp.Client with provided host and ssh.clientConfig
@@ -10,5 +11,6 @@ func NewClient(host string, config *ssh.ClientConfig) Client {
 	return Client{
 		Host:         host,
 		ClientConfig: config,
+		Timeout:      time.Minute,
 	}
 }
