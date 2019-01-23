dfa5c0bd70793634c4af63a7566123d5613edcd5
Dan Enman <enmand@gmail.com>
2019-01-22 22:08:20 -0400

    Add CopyFromRemote(path string) (file io.Reader, permissions int, err error)
    Add a CopyFromRemote which returns an io.Reader with the
    contents of the remote file

--- client.go
+++ client.go
@@ -9,13 +9,14 @@ import (
 	"bytes"
 	"errors"
 	"fmt"
-	"golang.org/x/crypto/ssh"
 	"io"
 	"io/ioutil"
 	"os"
 	"path"
 	"sync"
 	"time"
+
+	"golang.org/x/crypto/ssh"
 )
 
 type Client struct {
@@ -64,25 +65,94 @@ func waitTimeout(wg *sync.WaitGroup, timeout time.Duration) bool {
 		defer close(c)
 		wg.Wait()
 	}()
-	select {
-	case <-c:
-		return false // completed normally
-	case <-time.After(timeout):
-		return true // timed out
+
+	for {
+		select {
+		case <-c:
+			return false // completed normally
+		case <-time.After(timeout):
+			if timeout.Nanoseconds() == 0 {
+				continue
+			}
+			return true // timed out
+		}
 	}
+
+}
+
+// CopyFromRemote copies the contents of a remote file into an io.Reader
+func (a *Client) CopyFromRemote(remotePath string) (io.Reader, int, error) {
+	errCh := make(chan error, 2)
+
+	c := &Command{}
+	readerCh := make(chan io.Reader, 1)
+
+	r, _, w, err := pipes(a.Session)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	prepReader := func(wg *sync.WaitGroup) {
+		defer wg.Done()
+		defer w.Close()
+
+		err = readCommand(r, w, c)
+		if err != nil && err != io.EOF {
+			errCh <- err
+			return
+		}
+
+		// prep to recieve
+		_, err = w.Write([]byte{0})
+		if err != nil {
+			errCh <- err
+			return
+		}
+
+		// redirect our reader
+		readerCh <- r
+	}
+
+	syncFile := func(wg *sync.WaitGroup) {
+		defer wg.Done()
+
+		// start the transfer (and ignore error codes)
+		a.Session.Run("/usr/bin/scp -qf " + remotePath)
+	}
+
+	wg := a.scpFunc(prepReader, syncFile, errCh, 2)
+
+	if waitTimeout(wg, a.Timeout) {
+		return nil, 0, errors.New("timeout when downloading files")
+	}
+
+	close(errCh)
+	for err := range errCh {
+		if err != nil {
+			return nil, 0, err
+		}
+	}
+
+	return <-readerCh, c.Permissions, nil
 }
 
 // Copies the contents of an io.Reader to a remote location
 func (a *Client) Copy(r io.Reader, remotePath string, permissions string, size int64) error {
 	filename := path.Base(remotePath)
 	directory := path.Dir(remotePath)
-
-	wg := sync.WaitGroup{}
-	wg.Add(2)
-
 	errCh := make(chan error, 2)
 
-	go func() {
+	syncFile := func(wg *sync.WaitGroup) {
+		defer wg.Done()
+
+		err := a.Session.Run("/usr/bin/scp -qt " + directory)
+		if err != nil {
+			errCh <- err
+			return
+		}
+	}
+
+	copyFile := func(wg *sync.WaitGroup) {
 		defer wg.Done()
 		w, err := a.Session.StdinPipe()
 		if err != nil {
@@ -108,18 +178,11 @@ func (a *Client) Copy(r io.Reader, remotePath string, permissions string, size i
 			errCh <- err
 			return
 		}
-	}()
+	}
 
-	go func() {
-		defer wg.Done()
-		err := a.Session.Run("/usr/bin/scp -qt " + directory)
-		if err != nil {
-			errCh <- err
-			return
-		}
-	}()
+	wg := a.scpFunc(copyFile, syncFile, errCh, 2)
 
-	if waitTimeout(&wg, a.Timeout) {
+	if waitTimeout(wg, a.Timeout) {
 		return errors.New("timeout when upload files")
 	}
 	close(errCh)
@@ -134,4 +197,78 @@ func (a *Client) Copy(r io.Reader, remotePath string, permissions string, size i
 func (a *Client) Close() {
 	a.Session.Close()
 	a.Conn.Close()
+}
+
+func (a *Client) scpFunc(fileFunc func(*sync.WaitGroup), syncFunc func(*sync.WaitGroup), errCh chan error, wgSize int) *sync.WaitGroup {
+	wg := sync.WaitGroup{}
+	wg.Add(wgSize)
+
+	go func() {
+		fileFunc(&wg)
+	}()
+
+	go func() {
+		syncFunc(&wg)
+	}()
+
+	return &wg
+}
+
+// readCommand readies a buffer and initiates listening for an SCP command, and returns
+// the Command when parsed. w is not closed by readCommand
+func readCommand(r io.Reader, w io.WriteCloser, c *Command) error {
+	// recieve remote command input
+	buf := bytes.Buffer{}
+
+	// write a null byte to signal we are ready to receive data
+	_, err := w.Write([]byte{0})
+	if err != nil && err != io.EOF {
+		return err
+	}
+
+	for {
+		cmd := make([]byte, 64)
+		_, err := r.Read(cmd)
+		if err != nil && err != io.EOF {
+			return err
+		} else if err == io.EOF {
+			break
+		}
+
+		if string(cmd) == "" {
+			continue
+		}
+		_, err = buf.Write(cmd)
+		if err != nil {
+			return err
+		}
+
+		err = c.UnmarshalText(buf.Bytes())
+		if err != nil {
+			return err
+		}
+
+		return nil
+	}
+
+	return errors.New("no scp cmd found")
+}
+
+func pipes(s *ssh.Session) (io.Reader, io.Reader, io.WriteCloser, error) {
+	w, err := s.StdinPipe()
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	r, err := s.StdoutPipe()
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	e, err := s.StderrPipe()
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	return r, e, w, err
 }
