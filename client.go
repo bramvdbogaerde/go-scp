977ee74ac37b88c05448bc396934328e42b004a1
Bram Vandenbogaerde <bram@vdb.space>
2022-12-20 00:07:48 +0100

    Revert "remove superfluous call to io.Writer.Close()"
    This reverts commit df1131e21d691ed7b2a4a70749312542805e69d6.

--- client.go
+++ client.go
@@ -147,6 +147,7 @@ func (a *Client) CopyPassThru(ctx context.Context, r io.Reader, remotePath strin
 	if err != nil {
 		return err
 	}
+	defer w.Close()
 
 	if passThru != nil {
 		r = passThru(r, size)
